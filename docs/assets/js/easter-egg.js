c3c4d879fdc8932f21e8492c8ad48fc920c9ee2a
Mehdi Nassim KHODJA <18899702+naskio@users.noreply.github.com>
2022-11-28 15:34:17 +0100

    Cleaning repository

--- docs/assets/js/easter-egg.js
+++
@@ -1,8 +0,0 @@
-function easteregg() {
-    const span = document.getElementById('easter-egg');
-    if (span.textContent === 'ö') {
-        span.textContent = 'œ';
-    } else if (span.textContent === 'œ') {
-        span.textContent = 'ö';
-    }
-}
